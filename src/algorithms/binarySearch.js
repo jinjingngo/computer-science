6b7b23ed3e7c155989cfe5661653f070938ba088
charleserious <wujinjing25@gmail.com>
2020-09-12 00:06:34 +0800

    Chanllenge: Binary search

--- src/algorithms/binarySearch.js
+++ src/algorithms/binarySearch.js
@@ -1,4 +1,17 @@
 const binarySearch = (array, target) => {
+  let min = 0;
+  let max = array.length - 1;
+  let guess;
+  while (min <= max) {
+    guess = Math.floor((min + max) / 2);
+    if (array[guess] === target) {
+      return guess;
+    } else if (array[guess] < target) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
+    }
+  }
   return -1;
 };
 
--- test/algorithms/binarySearch.spec.js
+++ test/algorithms/binarySearch.spec.js
@@ -2,9 +2,25 @@ const { expect } = require('chai');
 const binarySerch = require('../../src/algorithms/binarySearch');
 
 describe('binarySearch', function () {
-  it('found nothing', function () {
-    const array = [];
-    const target = 1;
-    expect(binarySerch(array, target)).equal(-1);
+  let primes;
+  before(function () {
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+      41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+  });
+  it('no such prime `12`', function () {
+    const result = binarySerch(primes, 12);
+    expect(result).equal(-1);
+  });
+  it('found 73 at 20', function () {
+    const result = binarySerch(primes, 73);
+    expect(result).equal(20);
+  });
+  it('found 41 at 12', function () {
+    const result = binarySerch(primes, 41);
+    expect(result).equal(12);
+  });
+  it('found 2 at 0', function () {
+    const result = binarySerch(primes, 2);
+    expect(result).equal(0);
   });
 });
